cca032deb8b7099e0fd023927a5bcd69761be38b
L-Edgar <lelishaedgar10@gmail.com>
2024-02-04 23:33:21 +0300

    second

---
+++ app/src/main/java/edgar/example/elijahcollections/Activity/CartActivity.java
@@ -0,0 +1,16 @@
+package edgar.example.elijahcollections.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edgar.example.elijahcollections.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
